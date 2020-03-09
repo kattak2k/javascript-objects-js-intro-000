2e7e04ff00f6359b5ea8b79c5779ac2725ae031d
Ravi <kkiran.ror@gmail.com>
2020-03-09 04:05:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist ={"mau": "udiki kaki"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-
+  object.assign({},playlist,{artistName: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName){
