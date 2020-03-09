8f58b058ca04303256387aea09f1d7db5c6a317f
Ravi <kkiran.ror@gmail.com>
2020-03-09 04:20:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,9 @@ playlist['Slowdive'] = 'Alison'
 playlist['My Bloody Valentine'] = 'Sometimes'
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, { `'${artistName}'`: songTitle})
+  //return Object.assign({}, playlist, {`${artistName}`: songTitle})
+     playlist[`${artistName}`] = songTitle
+     return playlist
 }
 
 function removeFromPlaylist(playlist, artistName){
